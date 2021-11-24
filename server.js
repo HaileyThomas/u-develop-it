45651e19c918c6c3dd07f0c135d47c67c2b28faf
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-24 09:12:25 -0700

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express');
+// import mysql2
+const mysql = require('mysql2');
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username
+        user: 'root',
+        // your mysql password
+        password: 'H!0r07t87',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
