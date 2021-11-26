53cbea7f07396acf9d938ea27302faa952ee75a1
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-25 17:30:30 -0700

    refactor routes

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
-const router = require('./routes/apiRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-router.use(require('./routes/voterRoutes'));
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
